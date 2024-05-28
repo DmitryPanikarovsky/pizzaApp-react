15e10b7fd46bad208591ab7cdce69b427a7010fe
DmitryPanikarovsky <dmitry.panikarovsky@yandex.ru>
2024-05-28 11:56:37 +0300

    ---

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,5 @@
 import { ProductCardProps } from './ProductCard.types'
 import styles from './ProductCard.module.scss'
-import { Link } from 'react-router-dom'
 import { MouseEvent } from 'react'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../store/store'
@@ -16,26 +15,24 @@ export function ProductCard( props: ProductCardProps ) {
     }
 
     return (
-        <Link to={`/product/${props.id}`} className={styles.link}>
-            <div className={styles.card}>
-                <div className={styles['card__head']} style={{ backgroundImage: `url(${props.image})` }}>
-                    <div className={styles['card__price']}>
-                        {props.price}&nbsp;
-                        <span className={styles['card__currency']}>₽</span>
-                    </div>
-                    <button className={styles['card__add-to-cart']} onClick={addToCart}>
-                        <img src="/public/add-to-cart.svg" alt="Добавить в корзину" />
-                    </button>
-                    <div className={styles['card__rating']}>
-                        {props.rating}
-                        <img src="/public/star-icon.svg" alt="Иконка рейтинга" />
-                    </div>
+        <div className={styles.card}>
+            <div className={styles['card__head']} style={{ backgroundImage: `url(${props.image})` }}>
+                <div className={styles['card__price']}>
+                    {props.price}&nbsp;
+                    <span className={styles['card__currency']}>₽</span>
                 </div>
-                <div className={styles['card__footer']}>
-                    <div className={styles['card__title']}>{props.name}</div>
-                    <div className={styles['card__description']}>{props.description}</div>
+                <button className={styles['card__add-to-cart']} onClick={addToCart}>
+                    <img src="/public/add-to-cart.svg" alt="Добавить в корзину" />
+                </button>
+                <div className={styles['card__rating']}>
+                    {props.rating}
+                    <img src="/public/star-icon.svg" alt="Иконка рейтинга" />
                 </div>
             </div>
-        </Link>
+            <div className={styles['card__footer']}>
+                <div className={styles['card__title']}>{props.name}</div>
+                <div className={styles['card__description']}>{props.description}</div>
+            </div>
+        </div>
     )
 }
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, defer } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { Layout } from '../layout/Layout'
 import { Cart } from '../pages/Cart/Cart'
 import { Error as ErrorPage } from '../pages/Error/Error'
-import { Product } from '../pages/Product/Product'
-import axios from 'axios'
-import { PREFIX } from '../helpers/API'
+// import { Product } from '../pages/Product/Product'
+// import axios from 'axios'
+// import { PREFIX } from '../helpers/API'
 import { Suspense, lazy } from 'react'
 import { AuthLayout } from '../layout/Auth/AuthLayout'
 import { Login } from '../pages/Login/Login'
@@ -31,20 +31,20 @@ export const routes = createBrowserRouter([
                 path: '/success',
                 element: <Success />
             },
-            {
-                path: '/product/:id',
-                element: <Product/>,
-                errorElement: 'Error!',
-                loader: async ({ params }) => {
-                    return defer({
-                        data: new Promise((resolve, reject) => {
-                            setTimeout(() => {
-                                axios.get(`${PREFIX}/products/${params.id}`).then(data => resolve(data)).catch(e => reject(e))
-                            }, 0)
-                        })
-                    })
-                }
-            }
+            // {
+            //     path: '/product/:id',
+            //     element: <Product/>,
+            //     errorElement: 'Error!',
+            //     loader: async ({ params }) => {
+            //         return defer({
+            //             data: new Promise((resolve, reject) => {
+            //                 setTimeout(() => {
+            //                     axios.get(`${PREFIX}/products/${params.id}`).then(data => resolve(data)).catch(e => reject(e))
+            //                 }, 0)
+            //             })
+            //         })
+            //     }
+            // }
         ]
     },
     {
